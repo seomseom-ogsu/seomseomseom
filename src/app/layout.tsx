f8414cc6a100f6f4f7e14f94308415199ddba72a
Park Nayeon <within197@naver.com>
2024-07-07 19:45:10 +0900

    chore: 리액트쿼리 셋팅

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import Provider from '../../providers/provider';
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -11,8 +12,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="ko">
+      <body>
+        <Provider>
+          <div>{children}</div>
+        </Provider>
+      </body>
     </html>
   );
 }
